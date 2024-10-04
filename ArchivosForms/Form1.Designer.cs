5e950f611823d5d2233f619b0578cd75f34404a7
Enrique990 <voidenrique@gmail.com>
2024-10-04 08:47:19 -0600

    Fin de la clase

---
+++ ArchivosForms/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ArchivosForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblGenero = new System.Windows.Forms.Label();
+            this.lblNota = new System.Windows.Forms.Label();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbGenero = new System.Windows.Forms.TextBox();
+            this.tbNota = new System.Windows.Forms.TextBox();
+            this.tbEdad = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(56, 40);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(121, 16);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Ingrese un nombre:";
+            // 
+            // lblGenero
+            // 
+            this.lblGenero.AutoSize = true;
+            this.lblGenero.Location = new System.Drawing.Point(56, 194);
+            this.lblGenero.Name = "lblGenero";
+            this.lblGenero.Size = new System.Drawing.Size(115, 16);
+            this.lblGenero.TabIndex = 1;
+            this.lblGenero.Text = "Ingrese el genero:";
+            // 
+            // lblNota
+            // 
+            this.lblNota.AutoSize = true;
+            this.lblNota.Location = new System.Drawing.Point(56, 144);
+            this.lblNota.Name = "lblNota";
+            this.lblNota.Size = new System.Drawing.Size(98, 16);
+            this.lblNota.TabIndex = 2;
+            this.lblNota.Text = "Ingrese la nota:";
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Location = new System.Drawing.Point(56, 86);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(104, 16);
+            this.lblEdad.TabIndex = 3;
+            this.lblEdad.Text = "Ingrese la edad:";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(202, 37);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(183, 22);
+            this.tbNombre.TabIndex = 4;
+            this.tbNombre.TextChanged += new System.EventHandler(this.tbNombre_TextChanged);
+            // 
+            // tbGenero
+            // 
+            this.tbGenero.Location = new System.Drawing.Point(202, 194);
+            this.tbGenero.Name = "tbGenero";
+            this.tbGenero.Size = new System.Drawing.Size(183, 22);
+            this.tbGenero.TabIndex = 6;
+            this.tbGenero.TextChanged += new System.EventHandler(this.tbGenero_TextChanged);
+            // 
+            // tbNota
+            // 
+            this.tbNota.Location = new System.Drawing.Point(202, 144);
+            this.tbNota.Name = "tbNota";
+            this.tbNota.Size = new System.Drawing.Size(183, 22);
+            this.tbNota.TabIndex = 7;
+            this.tbNota.TextChanged += new System.EventHandler(this.tbNota_TextChanged);
+            // 
+            // tbEdad
+            // 
+            this.tbEdad.Location = new System.Drawing.Point(202, 86);
+            this.tbEdad.Name = "tbEdad";
+            this.tbEdad.Size = new System.Drawing.Size(183, 22);
+            this.tbEdad.TabIndex = 8;
+            this.tbEdad.TextChanged += new System.EventHandler(this.tbEdad_TextChanged);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(310, 238);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 27);
+            this.btnAgregar.TabIndex = 9;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(59, 293);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(326, 356);
+            this.listBox1.TabIndex = 10;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 675);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.tbEdad);
+            this.Controls.Add(this.tbNota);
+            this.Controls.Add(this.tbGenero);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.lblNota);
+            this.Controls.Add(this.lblGenero);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblGenero;
+        private System.Windows.Forms.Label lblNota;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbGenero;
+        private System.Windows.Forms.TextBox tbNota;
+        private System.Windows.Forms.TextBox tbEdad;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ ArchivosForms/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArchivosForms
+{
+    public partial class Form1 : Form
+    {
+        private const string filePath = "datos.dat";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    using (FileStream mArchivoLector = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                    using (BinaryReader Lector = new BinaryReader(mArchivoLector))
+                    {
+                        while (mArchivoLector.Position != mArchivoLector.Length)
+                        {
+                            int length = Lector.ReadInt32();
+                            char[] nombreArray = Lector.ReadChars(length);
+                            string nombre = new string(nombreArray);
+                            int edad = Lector.ReadInt32();
+                            int nota = Lector.ReadInt32();
+                            char genero = Lector.ReadChar();
+
+                            listBox1.Items.Add($"Nombre: {nombre}, Edad: {edad}, Nota: {nota}, Género: {genero}");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar los datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void tbNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbEdad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbNota_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbGenero_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(tbNombre.Text) ||
+                    string.IsNullOrWhiteSpace(tbEdad.Text) ||
+                    string.IsNullOrWhiteSpace(tbNota.Text) ||
+                    string.IsNullOrWhiteSpace(tbGenero.Text))
+                {
+                    MessageBox.Show("Por favor, complete todos los campos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                string nombre = tbNombre.Text;
+                int edad = int.Parse(tbEdad.Text);
+                int nota = int.Parse(tbNota.Text);
+                char genero = char.Parse(tbGenero.Text);
+
+                if (tbGenero.Text.Length != 1)
+                {
+                    MessageBox.Show("El campo género debe contener solo un carácter.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                using (FileStream mArchivoEscritor = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write))
+                using (BinaryWriter Escritor = new BinaryWriter(mArchivoEscritor))
+                {
+                    mArchivoEscritor.Seek(0, SeekOrigin.End);
+
+                    Escritor.Write(nombre.Length);
+                    Escritor.Write(nombre.ToCharArray());
+                    Escritor.Write(edad);
+                    Escritor.Write(nota);
+                    Escritor.Write(genero);
+                }
+
+                listBox1.Items.Add($"Nombre: {nombre}, Edad: {edad}, Nota: {nota}, Género: {genero}");
+
+                tbNombre.Clear();
+                tbEdad.Clear();
+                tbNota.Clear();
+                tbGenero.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al agregar los datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
